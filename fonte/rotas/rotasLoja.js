af5b8333b1926aa4e1b014b7918eb5bd6602e892
Andre Kruss <andre.kruss@pdvseven.com.br>
2021-04-05 09:54:59 -0300

    Falta implementar patch

--- fonte/rotas/rotasLoja.js
+++ fonte/rotas/rotasLoja.js
@@ -12,13 +12,21 @@ router.get('/', (req, res) => {
 });
 
 // get lojas por chave
-router.get('/carregar/:chaveIdentificacao', (req, res) => {
-    const chaveIdentificacao = req.params.chaveIdentificacao;
-    Loja.findOne({chaveIdentificacao: chaveIdentificacao})
+router.get('/carregar/:chave', (req, res) => {
+    const chave = req.params.chave;
+    Loja.findOne({chaveIdentificacao: {$elemMatch: {chave: chave}}})
         .exec()
         .then(doc => {
             console.log("Do banco de dados:", doc);
-            res.status(200).json(doc);
+
+            let resultado = [];
+            for(i = 0; i < doc.chaveIdentificacao.length; i++){
+                if (doc.chaveIdentificacao[i].chave === chave){
+                    resultado = doc.chaveIdentificacao[i];
+                }
+            }
+            
+            res.status(200).json(resultado);
         })
         .catch(err => {
             console.log(err);
@@ -27,9 +35,9 @@ router.get('/carregar/:chaveIdentificacao', (req, res) => {
 });
 
 // Listar lojas
-router.get('/listar/:chaveIdentificacao', (req, res) => {
-    const chaveIdentificacao = req.params.chaveIdentificacao;
-    Loja.find({chaveIdentificacao: chaveIdentificacao})
+router.get('/listar/:idProprietario', (req, res) => {
+    const idProprietario = req.params.idProprietario;
+    Loja.find({idProprietario: idProprietario})
         .exec()
         .then(doc => {
             console.log("Do banco de dados:", doc);
@@ -44,18 +52,18 @@ router.get('/listar/:chaveIdentificacao', (req, res) => {
 // post para
 router.post('/adicionar', (req, res) => {
 
-    let chaves = req.body.chaveIdentificacao;
-    let chavesFormatadas = [];
+    let chavesArray = req.body.chaveIdentificacao;
 
-    for(i = 0; i < chaves.length; i++){
-        chavesFormatadas.push(chaves[i].split(' ').join('-'));
+    for(i = 0; i < chavesArray.length; i++){
+        //chavesFormatadas.push(chaves[i].split(' ').join('-'));
+        chavesArray[i].chave = chavesArray[i].chave.split(' ').join('-');
     }
 
     const loja = new Loja({
         _id: mongoose.Types.ObjectId(),
         idProprietario: req.body.idProprietario,
         idPessoa: req.body.idPessoa,
-        chaveIdentificacao: chavesFormatadas,
+        chaveIdentificacao: chavesArray,
         idEndereco: req.body.idEndereco,
         telefones: req.body.telefones,
         entregaBairro: req.body.entregaBairro,
@@ -76,16 +84,20 @@ router.post('/adicionar', (req, res) => {
         });
 });
 
-// alterar loja
-router.patch('/alterar/:chaveIdentificacao', (req, res) => {
-    const chaveIdentificacao = req.params.chaveIdentificacao; 
+/*router.patch('/alterar/:id/:chave', (req, res) => {
+    const id = req.params.id;
+    const chave = req.params.chave; 
     const alteracoes = {};
 
-    for(const [chave, valor] of Object.entries(req.body)){
-        alteracoes[chave] = valor;
+    for(const [ch, val] of Object.entries(req.body)){
+        alteracoes[ch] = val;
     }
 
-    Loja.update({chaveIdentificacao: chaveIdentificacao}, { $set: alteracoes})
+    Loja.update(
+        {_id: id}, 
+        { $set: alteracoes},
+        {arrayFilters: [{chaveIdentificacao.chave: chave }]}
+    )
     .exec()
         .then(doc => {
             console.log("Do banco de dados:", doc);
@@ -96,5 +108,33 @@ router.patch('/alterar/:chaveIdentificacao', (req, res) => {
             res.status(500).json({error: err});
         });
 })
+
+// alterar loja
+/*
+router.patch('/alterar/:id/:chave', (req, res) => {
+    const id = req.params.id;
+    const chave = req.params.chave; 
+    const alteracoes = {};
+
+    for(const [ch, val] of Object.entries(req.body)){
+        alteracoes[ch] = val;
+    }
+
+    Loja.update(
+        {_id: id}, 
+        { $set: alteracoes},
+        {arrayFilters: [{chave: chave }]}
+    )
+    .exec()
+        .then(doc => {
+            console.log("Do banco de dados:", doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+})
+*/
 
 module.exports = router;
